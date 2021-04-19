bf97041ba7653fe01ae746ebd8186fec4f7de453
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2021-04-19 22:27:11 +0200

    Add files via upload

---
+++ Banken/Banken/Operations.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Banken
+{
+    class Operations
+    {
+        public bool loggedIn = false;
+        public void WithDraw(int amnt)
+        {
+
+        }
+
+        public void Deposit(string usrName,int amnt)
+        {
+            try
+            {
+                using (StreamWriter deposit = new StreamWriter(usrName + ".txt"))
+                {
+
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+        public void CreateAccount(string usrName, string passWrd)
+        {
+            try
+            {
+                string fileName = usrName + ".txt";
+                using (StreamWriter createAcc = new StreamWriter(fileName))
+                {
+                    createAcc.WriteLine("#\r\n" + passWrd);
+                    createAcc.Close();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Misslyckades att skapa konto.");
+                Task.Delay(1500);
+            }
+        }
+        public bool kontoExist(string usrName)
+        {
+            try
+            {
+                using (StreamReader ifKontoExist = new StreamReader(usrName + ".txt"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public double getSaldo(string usrName)
+        {
+            try
+            {
+                StreamReader readSaldo = new StreamReader(usrName + ".txt");
+                string saldoRead = readSaldo.ReadLine();
+                while (!saldoRead.StartsWith("saldo:"))
+                {
+                    saldoRead = readSaldo.ReadLine();
+                    if (saldoRead.StartsWith("saldo:"))
+                    {
+                        saldoRead = readSaldo.ReadLine();
+                        double returnValue = Convert.ToDouble(saldoRead);
+                        return returnValue;
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+            return 0;
+        }
+
+        public void Login(string userName, string passWrd)
+        {
+            try
+            {
+                StreamReader login = new StreamReader(userName + ".txt");
+                string pwdCheck = login.ReadLine();
+                while (pwdCheck != null)
+                {
+                    if (pwdCheck.StartsWith("#"))
+                    {
+                        pwdCheck = login.ReadLine();
+                        if (pwdCheck == passWrd)
+                        {
+                            Console.WriteLine("Välkommen: " + userName);
+                            loggedIn = true;
+                            Thread.Sleep(1500);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fel lösenord.");
+                            break;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Användare finns ej.");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Banken/Banken/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] kontoNr = new int[10];
+            //int kontoNummer;
+            //string konto = "";
+            bool loggedIn = false;
+            string usrName;
+            string passWrd;
+            string forNamn;
+            Random rnd = new Random();
+            Operations operations = new Operations();
+            while (true)
+            {
+                while (loggedIn == false)
+                {
+                    Console.WriteLine("Välkommen till banken!");
+                    Console.WriteLine("1. Logga in");
+                    Console.WriteLine("2. Skapa nytt konto");
+                    Console.WriteLine("3. FAQ");
+                    string val = Console.ReadLine();
+                    int oVal = Convert.ToInt32(val);
+                    switch (oVal)
+                    {
+                        case 1:
+                            Console.WriteLine("Logga in;");
+                            Console.Write("Användarnamn: ");
+                            usrName = Console.ReadLine();
+                            Console.Write("Lösenord: ");
+                            passWrd = Console.ReadLine();
+                            try
+                            {
+                                operations.Login(usrName, passWrd);
+                                loggedIn = operations.loggedIn;
+                            }
+                            catch
+                            {
+                                break;
+                            }
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Skapa nytt konto; ");
+                            /*string usrName = Console.ReadLine();
+                            for (int i = 0; i < 8; i++)
+                            {
+                                kontoNr[i] = rnd.Next(1, 9);
+                                Thread.Sleep(500);
+                                Console.Write(kontoNr[i]);
+                                konto += kontoNr[i].ToString();
+                            }
+                            //kontoNummer = Convert.ToInt32(konto);
+                            //Console.WriteLine("\r\nDitt kontonummer är: " + kontoNummer);
+                            Console.ReadLine();
+                            */
+                            Console.Write("Användarnamn: ");
+                            usrName = Console.ReadLine();
+                            Console.Write("Lösenord: ");
+                            passWrd = Console.ReadLine();
+                            Console.Write("Förnamn: ");
+                            forNamn = Console.ReadLine();
+                            operations.CreateAccount(usrName, passWrd);
+                            break;
+
+                        case 3:
+                            break;
+
+
+
+                    }
+                }
+                while (loggedIn == true)
+                {
+                    Console.WriteLine(" bank");
+                    Console.WriteLine("Kontobelopp: ");
+                    Console.WriteLine("Vad vill du göra?");
+                    Console.WriteLine("1. Ta ut pengar.");
+                    Console.WriteLine("2. Sätta in pengar.");
+                    Console.WriteLine("3. Logga ut.");
+                    string val = Console.ReadLine();
+                    int oVal = Convert.ToInt32(val);
+                    switch (oVal)
+                    {
+                        case 1:
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            loggedIn = false;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
