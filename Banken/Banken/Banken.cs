766bc45f661a7d1a6619e6c9401257eea4012f81
JonasCarlstroem <82809067+JonasCarlstroem@users.noreply.github.com>
2021-04-19 11:55:56 +0200

    Add files via upload

---
+++ Banken/Banken/Banken.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Banken
+{
+    class Bank
+    {
+        public struct Konto
+        {
+            public int kontoNr { get; set; }
+            public string userName { get; set; }
+            public string passWord { get; set; }
+            public string forNamn { get; set; }
+            public int saldo { get; set; }
+        }
+
+        public void nyttKonto(int kontoNr, string userName, string passWord, string forNamn, int saldo)
+        {
+            Konto konto = new Konto();
+            konto.kontoNr = kontoNr;
+            konto.userName = userName;
+            konto.passWord = passWord;
+            konto.forNamn = forNamn;
+            konto.saldo = saldo;
+        }
+    }
+}
+
